b3445dc4019a89f41480600dc8cd6f4dee2abddd
yanrdgs-dev <yansantosrodrigues.dev@gmail.com>
2025-05-15 15:13:12 -0300

    fix: trocando merge para ficar coerente com o do john

--- eda1/implementacoes/mergesort.c
+++ eda1/implementacoes/mergesort.c
@@ -4,34 +4,32 @@
 void merge (int *v, int e, int m, int d) {
     int *aux = malloc((d - e + 1) * sizeof(int)); // vetor auxiliar
 
-    for (int i = e; i <= d; i++) {
-        aux[i - e] = v[i]; // copia tudo pro vetor auxiliar
-    }
-
     int i = e; // declara indices p poder manipular
     int j = m + 1;
     int k = e;
 
     while (i <= m && j <= d) { // enquanto nenhuma das duas metades já foi iterada ou de (e..m) ou de (m+1..d)
-        if (aux[i - e] <= aux[j - e]) { // se v[i] for menor ou igual a v[j] (<= mantém a ordem, algoritmo estavel),
-            v[k] = aux[i - e]; // o v[k], ou seja, o indice k do v vira o v[i]
+        if (v[i] <= v[j]) { // se v[i] for menor ou igual a v[j] (<= mantém a ordem, algoritmo estavel),
+            aux[k - e] = v[i]; // o v[k], ou seja, o indice k do v vira o v[i]
             i++; // incrementa o i pq passou por ele já
         } else {
-            v[k] = aux[j - e]; // indice k do v vira o v[j]
+            aux[k - e] = v[j]; // indice k do v vira o v[j]
             j++; // incrementa o j, idem
         }
         k++; // incrementa o k no final pois significa que aquela posição já ta ordenada
     }
 
     while (i <= m) { // se entra nisso significa q a segunda metade foi toda iterada primeiro, então ainda falta elemento da primeira
-        v[k] = aux[i - e];
-        i++, k++;
+        aux[k - e] = v[i];
+        k++, i++;
     }
     
     while (j <= d) { // idem: linha 26 porem ao contrario
-        v[k] = aux[j - e];
-        j++, k++;
+        aux[k - e] = v[j];
+        k++, j++;
     }
+
+    for (k=0, i=e; i <= d; k++, i++) v[i] = aux[k];
 
     free(aux); // limpa vetor auxiliar p nao dar memory leak
 }
